cf61d5d56a0c47275e551db8f869a33b00519f7e
Alex Sokolsky <asokolsky@users.noreply.github.com>
2022-02-15 17:13:46 -0800

    Create escape.py

---
+++ programming/escape.py
@@ -0,0 +1,31 @@
+#
+# Console output embellished with the following color chars
+#
+class Escapes(str, Enum):
+    '''
+    Escape characters an app, e.g. terraform, can use to color console output
+    '''
+    red = "\x1b[31m"
+    reset = "\x1b[0m"
+    bright = "\x1b[1m"
+
+    @classmethod
+    def unescape(cls, inp:str) -> str:
+        '''
+        Remove escape chars from string
+        '''
+        out: List[str] = []
+
+        while inp:
+            found = ''
+            for _, strvalue in cls._value2member_map_.items():
+                if inp.startswith(strvalue):
+                    found = strvalue
+                    break
+            if found:
+                inp = inp[len(found):]
+            else:
+                out.append(inp[0:1])
+                inp = inp[1:]
+        return ''.join(out)
+    
