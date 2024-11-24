d4be20af668834dd00301f6c87e3a86bdc7be94f
Alex Sokolsky <asokolsky@gmail.com>
2024-11-24 15:18:59 -0800

    fix: font awesome

--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -5,17 +5,18 @@ codeBlocks.forEach(function (codeBlock) {
   copyButton.type = 'button';
   copyButton.ariaLabel = 'Copy code to clipboard';
   //copyButton.innerText = 'Copy';
-  copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>';
+  copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>Copy';
   codeBlock.append(copyButton);
 
   copyButton.addEventListener('click', function () {
     var code = codeBlock.querySelector('code').innerText.trim();
     window.navigator.clipboard.writeText(code);
     //copyButton.innerText = 'Copied';
-    copyButton.innerHtml = '<i class="fa fa-solid fa-check" aria-hidden="true"></i>';
+    copyButton.innerHtml = '<i class="fa fa-solid fa-check" aria-hidden="true"></i>Copied';
     var fourSeconds = 4000;
     setTimeout(function () {
-        copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>';
+        //copyButton.innerText = 'Copy';
+        copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>Copy';
     }, fourSeconds);
   });
 });
