b42eeb043eb89cbae22a8c46ea7030cc4942ba24
Alex Sokolsky <asokolsky@gmail.com>
2024-11-24 15:33:03 -0800

    fix: font awesome

--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -4,19 +4,19 @@ codeBlocks.forEach(function (codeBlock) {
   //copyButton.className = 'copy-to-clipboard';
   copyButton.type = 'button';
   //copyButton.ariaLabel = 'Copy code to clipboard';
-  //copyButton.innerText = 'Copy';
-  copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>Copy';
+  copyButton.innerText = 'Copy';
+  //copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>Copy';
   codeBlock.append(copyButton);
 
   copyButton.addEventListener('click', function () {
     var code = codeBlock.querySelector('code').innerText.trim();
     window.navigator.clipboard.writeText(code);
-    //copyButton.innerText = 'Copied';
-    copyButton.innerHtml = '<i class="fa fa-solid fa-check" aria-hidden="true"></i>Copied';
+    copyButton.innerText = 'Copied';
+    //copyButton.innerHtml = '<i class="fa fa-solid fa-check" aria-hidden="true"></i>Copied';
     var fourSeconds = 4000;
     setTimeout(function () {
-        //copyButton.innerText = 'Copy';
-        copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>Copy';
+        copyButton.innerText = 'Copy';
+        //copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>Copy';
     }, fourSeconds);
   });
 });
