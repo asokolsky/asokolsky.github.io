2cc649068f8dcc5ce58a7f35eda95c507aba3a63
Alex Sokolsky <asokolsky@gmail.com>
2024-11-24 14:29:34 -0800

    css:fix

--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -1,15 +1,19 @@
-const codeBlocks = document.querySelectorAll('figure > pre > code');
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
-copyCodeButtons.forEach((copyCodeButton, index) => {
-    const code = codeBlocks[index].innerText;
-    copyCodeButton.addEventListener('click', () => {
-        window.navigator.clipboard.writeText(code);
-        copyCodeButton.classList.remove('fa-copy');
-        copyCodeButton.classList.add('fa-check');
+var codeBlocks = document.querySelectorAll('pre.highlight');
+codeBlocks.forEach(function (codeBlock) {
+  var copyButton = document.createElement('button');
+  copyButton.className = 'copy-to-clipboard';
+  copyButton.type = 'button';
+  copyButton.ariaLabel = 'Copy code to clipboard';
+  copyButton.innerText = 'Copy';
+  codeBlock.append(copyButton);
 
-        setTimeout(() => {
-            copyCodeButton.classList.remove('fa-check');
-            copyCodeButton.classList.add('fa-copy');
-        }, 2000);
-    });
+  copyButton.addEventListener('click', function () {
+    var code = codeBlock.querySelector('code').innerText.trim();
+    window.navigator.clipboard.writeText(code);
+    copyButton.innerText = 'Copied';
+    var fourSeconds = 4000;
+    setTimeout(function () {
+      copyButton.innerText = 'Copy';
+    }, fourSeconds);
+  });
 });
