75a436886edc848518cd4e511955173a6054df03
Alex Sokolsky <asokolsky@gmail.com>
2024-11-23 19:16:37 -0800

    fix: copy button style

--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -1,7 +1,7 @@
 var codeBlocks = document.querySelectorAll('pre.highlight');
 codeBlocks.forEach(function (codeBlock) {
     var copyButton = document.createElement('button');
-    copyButton.className = 'copy';
+    copyButton.className = 'copy-button';
     copyButton.type = 'button';
     copyButton.ariaLabel = 'Copy code to clipboard';
     copyButton.innerText = 'Copy';
