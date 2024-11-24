fda0076fa5453e7713dc8ce5f84c01980be47fba
Alex Sokolsky <asokolsky@gmail.com>
2024-11-24 15:42:39 -0800

    feat: new font in the button text

--- _posts/2024-11-24-font-awesome.md
+++ _posts/2024-11-24-font-awesome.md
@@ -6,3 +6,9 @@ tags: Industry
 Pretty amazing!
 
 https://docs.fontawesome.com/web/add-icons/how-to
+
+to add style, include in the hear:
+
+```html
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+```
--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -1,7 +1,7 @@
 var codeBlocks = document.querySelectorAll('pre.highlight');
 codeBlocks.forEach(function (codeBlock) {
   var copyButton = document.createElement('button');
-  //copyButton.className = 'copy-to-clipboard';
+  copyButton.className = 'copy-to-clipboard';
   copyButton.type = 'button';
   //copyButton.ariaLabel = 'Copy code to clipboard';
   copyButton.innerText = 'Copy';
@@ -11,7 +11,7 @@ codeBlocks.forEach(function (codeBlock) {
   copyButton.addEventListener('click', function () {
     var code = codeBlock.querySelector('code').innerText.trim();
     window.navigator.clipboard.writeText(code);
-    copyButton.innerText = 'Copied';
+    copyButton.innerText = '&#2713;';
     //copyButton.innerHtml = '<i class="fa fa-solid fa-check" aria-hidden="true"></i>Copied';
     var fourSeconds = 4000;
     setTimeout(function () {
