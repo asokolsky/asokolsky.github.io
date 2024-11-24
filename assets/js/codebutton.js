96c479be775d4922ae87ac32d00d93e548d84b7a
Alex Sokolsky <asokolsky@gmail.com>
2024-11-24 15:23:50 -0800

    fix: fa

--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -1,9 +1,9 @@
 var codeBlocks = document.querySelectorAll('pre.highlight');
 codeBlocks.forEach(function (codeBlock) {
   var copyButton = document.createElement('button');
-  copyButton.className = 'copy-to-clipboard';
+  //copyButton.className = 'copy-to-clipboard';
   copyButton.type = 'button';
-  copyButton.ariaLabel = 'Copy code to clipboard';
+  //copyButton.ariaLabel = 'Copy code to clipboard';
   //copyButton.innerText = 'Copy';
   copyButton.innerHtml = '<i class="fa fa-solid fa-copy" aria-hidden="true"></i>Copy';
   codeBlock.append(copyButton);
