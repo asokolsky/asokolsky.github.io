827e4f062ebb0f59cccca61f0bd6c886b93a2192
Alex Sokolsky <asokolsky@gmail.com>
2024-11-24 18:44:20 -0800

    fix: another try

--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -1,22 +1,22 @@
 var codeBlocks = document.querySelectorAll('pre.highlight');
 codeBlocks.forEach(function(codeBlock) {
-  var copyButton = document.createElement('p');
+  var copyButton = document.createElement('div');
   copyButton.className = 'copy-to-clipboard';
   //copyButton.type = 'button';
   //copyButton.ariaLabel = 'Copy code to clipboard';
   //copyButton.innerText = 'Copy';
-  copyButton.innerHtml = '<i class="fa fa-copy" aria-hidden="true"></i>Copy';
+  copyButton.innerHtml = '&nbsp;<i class="fa fa-copy" aria-hidden="true"></i>&nbsp;';
   codeBlock.append(copyButton);
 
   copyButton.addEventListener('click', function () {
     var code = codeBlock.querySelector('code').innerText.trim();
     window.navigator.clipboard.writeText(code);
     //copyButton.innerText = 'Copied';
-    copyButton.innerHtml = '<i class="fa fa-check" aria-hidden="true"></i>Copied';
+    copyButton.innerHtml = '&nbsp;<i class="fa fa-check" aria-hidden="true"></i>&nbsp;';
     var fourSeconds = 4000;
     setTimeout(function () {
         //copyButton.innerText = 'Copy';
-        copyButton.innerHtml = '<i class="fa fa-copy" aria-hidden="true"></i>Copy';
+        copyButton.innerHtml = '&nbsp;<i class="fa fa-copy" aria-hidden="true"></i>&nbsp;';
     }, fourSeconds);
   });
 });
