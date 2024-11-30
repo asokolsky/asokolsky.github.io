66e54dd4feff876865098de1ad94f2f6a93cf7b7
Alex Sokolsky <asokolsky@gmail.com>
2024-11-30 10:43:11 -0800

    fix: js

--- assets/js/codebutton.js
+++ assets/js/codebutton.js
@@ -5,21 +5,20 @@ codeBlocks.forEach(function(codeBlock) {
   copyButton.type = 'button';
   //copyButton.ariaLabel = 'Copy code to clipboard';
   //copyButton.innerText = 'Copy';
-  var copyHtml = '<i class="fa fa-copy" aria-hidden="true"></i>';
-  var checkHtml = '<i class="fa fa-check" aria-hidden="true"></i>';
-  //copyButton.innerHtml = copyHtml;
-  copyButton.innerText = 'Copy';
+  var copyHtml = '<i class="fa fa-copy" aria-hidden="true"></i> Copy';
+  copyButton.innerHtml = copyHtml;
+  //copyButton.innerText = 'Copy';
   codeBlock.append(copyButton);
 
   copyButton.addEventListener('click', function () {
     var code = codeBlock.querySelector('code').innerText.trim();
     window.navigator.clipboard.writeText(code);
-    //copyButton.innerHtml = checkHtml;
-    copyButton.innerText = 'Copied';
+    copyButton.innerHtml = '<i class="fa fa-check" aria-hidden="true"></i> Copied';
+    //copyButton.innerText = 'Copied';
     var fourSeconds = 4000;
     setTimeout(function () {
-        //copyButton.innerHtml = copyHtml;
-        copyButton.innerText = 'Copy';
+        copyButton.innerHtml = copyHtml;
+        //copyButton.innerText = 'Copy';
       }, fourSeconds);
   });
 });
