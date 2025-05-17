de72012108268dcab0d44b405fafcb376392217e
Alex Sokolsky <asokolsky@gmail.com>
2025-05-17 13:18:15 -0700

    more emacs updates

---
+++ apps/emacs/customization.md
@@ -0,0 +1,13 @@
+# My Emacs Customization
+
+I use
+
+* json-mode
+* yaml-mode
+* markdown-mode
+* [dracula-theme](https://draculatheme.com/emacs)
+
+Hence my `~/.emacs.d/init.el`:
+```
+{% include_relative init.el %}
+```
---
+++ apps/emacs/init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(json-mode yaml-mode markdown-mode dracula-theme)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
